b649e087e2d0d0941a2ea758587dadd079c57c05
Hong Jiang <j@1byte.io>
2019-01-03 18:34:11 +0800

    :lock: use cerbot to request ssl certificate

---
+++ letsencrypt/request-cert.sh
@@ -0,0 +1,10 @@
+sudo docker run -it --rm \
+-v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v ./letsencrypt/letsencrypt-site:/data/letsencrypt \
+-v /docker-volumes/var/log/letsencrypt:/var/log/letsencrypt \
+certbot/certbot \
+certonly --webroot \
+--email youremail@domain.com --agree-tos --no-eff-email \
+--webroot-path=/data/letsencrypt \
+-d test-us-west.leancloud.tk
